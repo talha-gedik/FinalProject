331d5fc1a9c296ec485db9242187af6ca2b596f6
Talha Mahmut Gedik <talhamahmutgedik@hotmail.com>
2021-02-03 23:47:19 +0300

    03.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,20 +1,32 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            //foreach (var product in productManager.GetByUnitPrice(40,100))  //Fiyatı 40 ile 100 arasında olanları filtreler.
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            //foreach (var product in productManager.GetAllByCategoryId(2))  //CategoryId'si 2 olan ürünleri listeler.
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            foreach (var product in productManager.GetAll())  //Bütün ürünleri listeler.
             {
                 Console.WriteLine(product.ProductName);
             }
-
         }
     }
 }
