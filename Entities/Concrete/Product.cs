5f8af04dbb9a7699b24a0571d8a074b806556a15
Talha Mahmut Gedik <talhamahmutgedik@hotmail.com>
2021-02-03 21:01:55 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity //public bu classa diğer katmanların da (business,data vs.) ulaşabilmesini sağlar.
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
